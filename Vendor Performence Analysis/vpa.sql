223bc4aad0df3c9c32789414c3852b0ecf66e357
Md Rakibul Hasan Shaon <shaonrakibul00@gmail.com>
2026-05-22 01:13:00 +0600

    Refactor SQL queries in vpa.sql for analysis
    Added queries to calculate inventory counts and join inventory data.

--- Vendor Performence Analysis/vpa.sql
+++ Vendor Performence Analysis/vpa.sql
@@ -3,10 +3,11 @@
 use vpa;
 
 select * from begin_inventory;
+select * from vendor_invoice;
 select * from end_inventory;
 select * from purchaseing_prices;
 select * from purchases;
-select * from vendor_invoice;
+
 
 select distinct store from begin_inventory;
 select distinct city from begin_inventory;
@@ -19,3 +20,9 @@ SELECT city, COUNT(*) AS inventory_count FROM begin_inventory GROUP BY city ORDE
 
 
 select sum(price) from begin_inventory;
+
+select count(onhand), sum(price), store, Brand from begin_inventory where City= 'FURNESS' group by store, brand;
+
+SELECT i.startdate, j.enddate, i.InventoryId, i.brand, i.price FROM begin_inventory AS i 
+JOIN end_inventory AS j ON i.InventoryId = j.InventoryId where i.Store= '34'and i.Price>=20 
+GROUP BY i.price, i.InventoryId, i.startdate, j.enddate, i.Brand ;
